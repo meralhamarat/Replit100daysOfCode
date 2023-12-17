af11317875825131e06b9531cd7a0a3a59141c0a
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-17 21:07:40 +0300

    Add files via upload

---
+++ Day07.py
@@ -0,0 +1,15 @@
+print("   \033[33m-----------------------------\033[0m")
+print("\033[36mSAHTE LEYLA İLE MECNUN FANI BULMA İNATOR\033[0m")
+print("   \033[33m-----------------------------\033[0m")
+
+favDizi = input("Dizideki hırsız arkadaşın adı nedir?")
+if favDizi == "yavuz":
+    tatlı = input("Hmm şanslı günündesin. Hadi bakalım, Mecnun'un enn sevdiği tatlının adı nedir?")
+    if tatlı == "tulumba":
+        serenad = input("hadi bunu bil! İlk sezonda Mecnun'un Leyla'ya serenad yaptığı arabesk şarkı nedir?")
+        if serenad == "sevdalılar beni anlar":
+                yavuz = input("Yavuz yeni tanıştığı kişilere kendi mesleğini ne olarak tanıtıyordu?")
+                if yavuz == "performans sanatçısı":
+                    print("Helal la hepsini bildin, Demek ki Leyla ile Mecnun senin için bir diziden daha fazlası...")
+else:
+    print("Piii, sen hiç leyla ile mecnun izlememişsin ki!")
