39ee9a6bc42579edb56ec4a0dcf8dbb6108781c9
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-18 23:59:33 +0300

    Create Day09.py

---
+++ Day09.py
@@ -0,0 +1,10 @@
+print("\033[34mGeneration-Finder-Inator\033[0m")
+print("\033[32m***********************\033[0m")
+year = int(input("What year were you born?"))
+if year <= 1946:
+    print("OMG, you are silent generation!")
+elif year  >= 1946 and  year <= 1964:
+    print("Hi BabyBoomer! You've seen it all and paved the way for the rest of us. Cheers to your wisdom!")
+elif year >= 1964 and year <= 1980:
+    print("What's up, Generation X!You guys rocked the '80s and '90s. Keep that rebellious spirit alive!")
+
