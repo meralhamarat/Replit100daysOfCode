cad45d4d9c398f1834cfeb9b6e2eac38dc235cd2
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-18 23:59:55 +0300

    Update Day09.py

--- Day09.py
+++ Day09.py
@@ -6,5 +6,7 @@ if year <= 1946:
 elif year  >= 1946 and  year <= 1964:
     print("Hi BabyBoomer! You've seen it all and paved the way for the rest of us. Cheers to your wisdom!")
 elif year >= 1964 and year <= 1980:
-    print("What's up, Generation X!You guys rocked the '80s and '90s. Keep that rebellious spirit alive!")
+    print("What's up, Generation X!You guys rocked the '80s and '90s. Keep that rebellious spirit alive!"
 
+
+          
