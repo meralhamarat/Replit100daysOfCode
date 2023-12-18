61fded1bfef416cc4412242fa733af887fdcac09
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-19 00:07:24 +0300

    Update Day09.py

--- Day09.py
+++ Day09.py
@@ -3,10 +3,13 @@ print("\033[32m***********************\033[0m")
 year = int(input("What year were you born?"))
 if year <= 1946:
     print("OMG, you are silent generation!")
-elif year  >= 1946 and  year <= 1964:
+elif year  >= 1947 and  year <= 1964:
     print("Hi BabyBoomer! You've seen it all and paved the way for the rest of us. Cheers to your wisdom!")
-elif year >= 1964 and year <= 1980:
-    print("What's up, Generation X!You guys rocked the '80s and '90s. Keep that rebellious spirit alive!"
-
-
-          
+elif year >= 1965 and year <= 1981:
+    print("What's up, Generation X!You guys rocked the '80s and '90s. Keep that rebellious spirit alive!")
+elif year >= 1982 and year <= 1995:
+    print("Hey Millennials, keep adulting like pros and turning avocado toast into a lifestyle.")
+elif year >= 1996:
+    print("Hey, Gen Z! TikTok much?")
+else:
+    print("Please try again.")
