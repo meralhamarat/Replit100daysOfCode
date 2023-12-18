f7b4af602dbd0fa381889b6a1b80163512dc58cd
Meral Hamarat <114569285+meralhamarat@users.noreply.github.com>
2023-12-18 23:24:07 +0300

    Add files via upload

---
+++ Day08.py
@@ -0,0 +1,48 @@
+print("Hello. Welcome to your daily affirmation generator.")
+name = input("What is your name?")
+day = input("What is the day of the week?")
+
+if name == "meral" or name == "Meral":
+    if day == "monday" or day == "Monday":
+        print("""Let's kick off the week with a 'Marvelous Monday'!
+    Embrace the challenges and turn them into opportunities. You've got this!""")
+    if day == "tuesday" or day == "Tuesday":
+        print("""It's 'Turbulent Tuesday.' Gear up for a bumpy ride and face the chaos.
+    Embrace the turmoil, knowing that better days might be on the horizon.""")
+    if day == "wednesday" or day == "Wednesday":
+        print("""Welcome to 'Whiz Wednesday'! Spark your creativity, overcome hurdles,
+    and let your brilliance shine through. Midweek magic is in the air!""")
+    if day == "thursday" or day == "Thursday":
+        print("""Troublesome Thursday' is here. Navigate through the challenges,
+    dodge the obstacles, and dream of a less complicated end to the week.""")
+    if day == "friday" or day == "Friday":
+        print("""Hooray for 'Fantastic Friday'! Celebrate your accomplishments, enjoy the journey,
+    and savor the sweet taste of success. The weekend is almost here!""")
+    if day == "saturday" or day == "Saturday":
+        print("""Say hello to 'Sunny Saturday'! Bask in the warmth of joy,
+    take a leisurely stroll through happiness, and recharge for the adventures ahead.""")
+    if day == "sunday" or day == "Sunday":
+        print("""It's 'Serene Sunday'! Reflect on your achievements, appreciate the beauty around you,
+    and recharge your spirit for a week filled with possibilities.""")
+elif name == "Irem" or name == "irem":
+    if day == "monday" or day == "Monday":
+        print("""Oh no, it's 'Moody Monday'! Brace yourself for the challenges ahead and hope 
+    for a speedy escape into a better mood. Remember, it's just the beginning""")
+    if day == "tuesday" or day == "Tuesday":
+        print("""It's 'Triumphant Tuesday'! Face the day with confidence and let your achievements shine.
+    Each step forward is a victory in itself.""")
+    if day == "wednesday" or day == "Wednesday":
+        print("""Welcome to 'Whirlwind Wednesday.' Hold on tight as the week spins you around.
+    Find your balance amidst the chaos and hope for calmer times.""")
+    if day == "thursday" or day == "Thursday":
+        print("""Thriving Thursday' is here! Embrace the positive vibes, express gratitude,
+    and pave the way for success. You're on the road to triumph!""")
+    if day == "friday" or day == "Friday":
+        print("""Oh dear, it's 'Frustrating Friday.' Anticipate obstacles and brace yourself for potential setbacks.
+    The weekend might bring relief, but for now, it's a struggle.""")
+    if day == "saturday" or day == "Saturday":
+        print("""Embrace the beauty of 'Sunny Saturday'! Let the day's warmth inspire joy,
+    relaxation, and the pursuit of happiness.""")
+    if day == "sunday" or day == "Sunday":
+        print("""It's 'Serene Sunday'! Reflect on your blessings, find peace in the moment,
+    and recharge for a week ahead filled with new experiences.""")
